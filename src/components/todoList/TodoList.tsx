19328804c9b2a531366fb0354d9ac21aca36a2a3
dongwonnn <kkum9408@gmail.com>
2021-08-26 13:03:45 +0900

    [#6] refactor: Drag and Drop 리팩토링
    - e type 제거
    - setTodoState 이용한 배열 재생성

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -4,43 +4,44 @@ import { Itodo } from 'utils/todoService';
 
 interface TodoListProps {
   todos: Itodo[];
-  setTodoState:(e:any)=>void
+  setTodoState: (e: Itodo[]) => void;
 }
 
-
 const TodoList = (props: TodoListProps) => {
-  const { todos,setTodoState } = props;
+  const { todos, setTodoState } = props;
 
-  const draggingItem = useRef<number|null>(null);
-  const dragOverItem = useRef<number|null>(null);
+  const draggingItem = useRef<number | null>(null);
+  const dragOverItem = useRef<number | null>(null);
 
-  const handleDragStart = (e:any, position:any)=> {
+  const handleDragStart = (position: number) => {
     draggingItem.current = position;
-   
-  }
+  };
 
-  const handleDragEnter = (e:any, position:any)=>{
+  const handleDragEnter = (position: number) => {
     dragOverItem.current = position;
-    
-    const todosCopy = todos&&[...todos];
-    
+
+    const todosCopy = todos && [...todos];
+
     const draggingItemContent = todosCopy[draggingItem.current!];
     todosCopy.splice(draggingItem.current!, 1);
     todosCopy.splice(dragOverItem.current!, 0, draggingItemContent);
     draggingItem.current = dragOverItem.current;
     dragOverItem.current = null;
     setTodoState(todosCopy);
-  }
+  };
+
   return (
     <>
       {todos &&
         todos.length > 0 &&
         todos?.map((todo, index) => (
-          <TodoItem key={todo.id} 
-          onDragStart={(e) => handleDragStart(e, index)} 
-          onDragEnter={(e) => handleDragEnter(e, index)} 
-          onDragOver={(e) => e.preventDefault()} 
-          draggable>
+          <TodoItem
+            key={todo.id}
+            onDragStart={() => handleDragStart(index)}
+            onDragEnter={() => handleDragEnter(index)}
+            onDragOver={(e) => e.preventDefault()}
+            draggable
+          >
             <div>{todo.taskName}</div>
             <p>{todo.importance}</p>
           </TodoItem>
