7c07869a06cac63e834a69db3457cc65a43df99c
dongwonnn <kkum9408@gmail.com>
2021-08-27 15:05:59 +0900

    chore: Header 컴포넌 트 분리

---
+++ src/components/header/Clock.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { DATE_FORM, TIME_FORM } from 'utils/constants';
+import styled from 'styled-components';
+
+const Clock = () => {
+  const [date] = useState(new Date().toLocaleDateString('en-US', DATE_FORM));
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString('en-US', TIME_FORM),
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString('en-US', TIME_FORM));
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <TodoHeadBlock>
+      <p>{date}</p>
+      <p>{time}</p>
+    </TodoHeadBlock>
+  );
+};
+
+export default React.memo(Clock);
+
+const TodoHeadBlock = styled.div`
+  padding-top: 52px;
+  border-bottom: 3px solid #33bb77;
+
+  p {
+    text-align: center;
+    color: #119955;
+    padding-top: 5px;
+  }
+
+  p:first-child {
+    font-size: 22px;
+  }
+  p:last-child {
+    font-size: 26px;
+  }
+`;
