55b22d26b405d61cbec9116e0f95d6a92ccfd8af
dongwonnn <kkum9408@gmail.com>
2021-08-27 21:54:54 +0900

    [#6] feat: Drag and Drop 기능 구현
    - 같은 status 안에서만 Drag and Drop 가능
    - throttle 이용한 이벤트 제어

--- src/components/todoList/ToDoItem.tsx
+++ src/components/todoList/ToDoItem.tsx
@@ -1,4 +1,10 @@
-import React,{useRef, useState, useEffect} from 'react';
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  forwardRef,
+  useCallback,
+} from 'react';
 import styled from 'styled-components';
 import { useTodosDispatch, Todo } from 'contexts/Todo/TodoStore';
 import { initStar } from 'utils/constants';
@@ -7,20 +13,20 @@ import { ReactComponent as StarSvg } from 'components/assets/svg/star.svg';
 import { ReactComponent as DeleteSvg } from 'components/assets/svg/delete.svg';
 import { ReactComponent as EditSvg } from 'components/assets/svg/edit.svg';
 import Status from '../common/Status';
+import { couldStartTrivia } from 'typescript';
 
 interface ToDoItemProps {
   todo: Todo;
-  tagName:string;
+  tagName: string;
+  setDragTodo: (e: Todo | null) => void;
 }
 
-const ToDoItem = (props: ToDoItemProps) => {
+const ToDoItem = forwardRef<HTMLInputElement, ToDoItemProps>((props, ref) => {
   const dispatch = useTodosDispatch();
-  const { todo, tagName } = props;
-
+  const { todo, tagName, setDragTodo } = props;
   const [isEdit, setIsEdit] = useState(false);
   const [status, setStatus] = useState('');
   const [starIndex, setStarIndex] = useState(initStar);
-
 
   const tasKNameRef = useRef(null);
   useEffect(() => {
@@ -28,21 +34,15 @@ const ToDoItem = (props: ToDoItemProps) => {
     if (updateTasKName) updateTasKName.focus();
   }, [isEdit]);
 
-
   const newStars = (index: number) =>
     initStar.map((_, i): boolean => i < index);
 
-  
-
   const handleRemove = (id: number) => {
-    if(window.confirm('정말 삭제하시겠습니까?'))
-      return(
-        dispatch({
-          type: 'REMOVE',
-          id: id,
-        })
-    )
-    
+    if (window.confirm('정말 삭제하시겠습니까?'))
+      return dispatch({
+        type: 'REMOVE',
+        id: id,
+      });
   };
 
   const handleEdit = () => {
@@ -58,88 +58,128 @@ const ToDoItem = (props: ToDoItemProps) => {
         createAt: todo.createAt,
         updateAt: new Date(),
       };
-      dispatch({type: 'UPDATE',
-      updateTodo: updateTodo,});
+      dispatch({ type: 'UPDATE', updateTodo: updateTodo });
     }
-    if (updateText === "") {
+    if (updateText === '') {
       updateTasKName.innerText = todo.taskName;
     }
     setIsEdit((prev) => !prev);
   };
 
+  const handleDragStart = useCallback(
+    (todo) => {
+      setDragTodo(todo);
+    },
+    [setDragTodo],
+  );
+
+  const handleDragEnd = useCallback(() => {
+    setDragTodo(null);
+  }, [setDragTodo]);
+
   return (
-    <TodoItemWrapper>
-      <TodoName ref={tasKNameRef} contentEditable={isEdit} suppressContentEditableWarning={true}>{todo.taskName}</TodoName>
+    <TodoItemWrapper
+      draggable
+      ref={ref}
+      onDragStart={() => handleDragStart(todo)}
+      onDragEnd={handleDragEnd}
+    >
+      <p>{todo.id}</p>
+      <TodoName
+        ref={tasKNameRef}
+        contentEditable={isEdit}
+        suppressContentEditableWarning={true}
+      >
+        {todo.taskName}
+      </TodoName>
       <ImpWrap>
-      <span>우선순위 :</span> <StarTag>
-        {newStars(todo.importance).map((item: boolean, index: number) =>
-          item ? <StarSvg key={index} fill="gold" /> : '',
-        )}</StarTag>
+        <span>우선순위 :</span>{' '}
+        <StarTag>
+          {newStars(todo.importance).map((item: boolean, index: number) =>
+            item ? <StarSvg key={index} fill="gold" /> : '',
+          )}
+        </StarTag>
       </ImpWrap>
-      
+
       <WriterWrap>
-      <span>작성자 : </span><TagWriter>{todo.writer}</TagWriter>
+        <span>작성자 : </span>
+        <TagWriter>{todo.writer}</TagWriter>
       </WriterWrap>
-      
+
       <TodoStatus>
-      <span>진행상황 : </span>{isEdit ? <Status status={todo.status} setStatus={setStatus}/> : 
-      <StatusRes status={todo.status}>{todo.status}</StatusRes>}
+        <span>진행상황 : </span>
+        {isEdit ? (
+          <Status status={todo.status} setStatus={setStatus} />
+        ) : (
+          <StatusRes status={todo.status}>{todo.status}</StatusRes>
+        )}
       </TodoStatus>
-      
-      {isEdit ? <p onClick={handleEdit}>저장</p> : <EditSvg onClick={handleEdit} />}
+
+      {isEdit ? (
+        <p onClick={handleEdit}>저장</p>
+      ) : (
+        <EditSvg onClick={handleEdit} />
+      )}
       <DeleteSvg onClick={() => handleRemove(todo.id)} />
     </TodoItemWrapper>
   );
-};
+});
 
 const TodoItemWrapper = styled.div`
   margin-bottom: 5px;
   border: 1px solid black;
-  border-radius:4px;
-  background-color:lightgray;
-  span{
-    margin-left:4px;
+  border-radius: 4px;
+  background-color: lightgray;
+  cursor: move;
+  span {
+    margin-left: 4px;
   }
 `;
 
 const TodoName = styled.div`
-display:flex;
-justify-content:center;
-padding:10px;
-`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+`;
 
 const WriterWrap = styled.div`
-display: flex;
-justify-content:space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const TagWriter = styled.span`
-margin-right:5px;
-margin-bottom:4px;
-padding:2px;
-background-color:aqua;
-border-radius:5px;`
+  margin-right: 5px;
+  margin-bottom: 4px;
+  padding: 2px;
+  background-color: aqua;
+  border-radius: 5px;
+`;
 
 const ImpWrap = styled.div`
-display: flex;
-justify-content:space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 const StarTag = styled.span`
-margin-right:5px;
-margin-bottom:4px;
-padding:2px;`
+  margin-right: 5px;
+  margin-bottom: 4px;
+  padding: 2px;
+`;
 
 const TodoStatus = styled.div`
-display: flex;
-justify-content:space-between;
-`
-const StatusRes = styled.span<{status:string}>`
-margin-right:5px;
-margin-bottom:4px;
-padding:2px;
-border-radius:4px;
-background-color:${props=>props.status==="To Do"?'yellow':props.status==="In Progress"?"green":"red"}
-`
-
+  display: flex;
+  justify-content: space-between;
+`;
+const StatusRes = styled.span<{ status: string }>`
+  margin-right: 5px;
+  margin-bottom: 4px;
+  padding: 2px;
+  border-radius: 4px;
+  background-color: ${(props) =>
+    props.status === 'To Do'
+      ? 'yellow'
+      : props.status === 'In Progress'
+      ? 'green'
+      : 'red'};
+`;
 
 export default ToDoItem;
